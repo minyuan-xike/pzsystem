8fbebe92ccf22893caf9797e7ae4dc4fb1b95ccc
Wang Xinyun <1528444542@qq.com>
2026-01-08 22:32:28 +0800

    first

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import{createRouter}from 'vue-router'
+
+import layout from '../views/Main.vue'
+import Login from '../views/login/index.vue'
+const router = [
+    {
+        path:'/',
+        component:layout
+    },
+    {
+        path:'/login',
+        component: 
+    }
+]
+
+createRouter({
+
+})
