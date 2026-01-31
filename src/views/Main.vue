9150145a839565e1110dcf67de94683bc580c70a
Wang Xinyun <1528444542@qq.com>
2026-01-31 22:47:50 +0800

    布局+aside组件创建

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,6 +1,15 @@
 <template>
-    <div>
-        layout
-    </div>
+  <div class="common-layout">
+    <el-container>
+      <!-- <el-aside width="200px">Aside</el-aside> -->
+      <Aside />
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
-<script setup></script>
+<script setup>
+import Aside from '../components/aside.vue';
+</script>
